f9c93b446b9ac80135e36336490ff48b97f6b8d6
vikramcse <vikramcse.10@gmail.com>
2016-01-16 17:11:32 +0530

    Added gulpfile for transformation

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp');
+var browserify = require('gulp-browserify');
+var concat = require('gulp-concat');
+var plumber = require('gulp-plumber');
+
+gulp.task('browserify', function() {
+    gulp.src('src/dashboard.js')
+        .pipe(plumber())
+        .pipe(browserify({transform: 'reactify', debug: true}))
+        .pipe(concat('dash.js'))
+        .pipe(gulp.dest('public'));
+});
+
+gulp.task('default', ['browserify']);
+
+gulp.task('watch', function() {
+    gulp.watch('src/**/*.*', ['default']);
+});
