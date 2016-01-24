30b35a12d455b6cdb7646d87ce82ec234be84a9f
vikramcse <vikramcse.10@gmail.com>
2016-01-24 19:46:41 +0530

    Removed routes

--- src/front.js
+++ src/front.js
@@ -1,4 +1,8 @@
 var React = require('react');
 var App = require('./components/front/App.js');
+var ReactRouter = require('react-router');
+var routes = require('./routes.js');
 
-React.render(<App />, document.getElementById('front'));
+ReactRouter.run(routes, ReactRouter.HistoryLocation, function(Root) {
+    React.render(<Root />, document.getElementById('front'));
+});
